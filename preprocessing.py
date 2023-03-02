bacb0b1ec3e17b072e323cf83d285b3c5510df20
Antony <meiyixuan2000@126.com>
2023-03-02 17:55:30 +0800

    start preprocessing

---
+++ preprocessing.py
@@ -0,0 +1,16 @@
+import os
+
+from qiskit.circuit import QuantumCircuit
+
+
+def pass1_reg_renaming(input_path, output_path):
+    with open(input_path, "r") as input_file:
+        with open(output_path, "w")
+
+
+def main():
+    pass1_reg_renaming(input_path="./test.qasm", output_path="pass1.qasm")
+
+
+if __name__ == '__main__':
+    main()
