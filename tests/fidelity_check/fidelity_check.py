bf8f0821f9750064c724e948548be4219899a88a
Antony <meiyixuan2000@126.com>
2023-03-29 10:58:56 +0800

    + add check for fidelity results

--- tests/fidelity_check/fidelity_check.py
+++ tests/fidelity_check/fidelity_check.py
@@ -119,6 +119,28 @@ def fidelity(filename):
         return fidelity_val
 
 
+def eh_fidelity(filename):
+    with open(filename, "r") as file:
+        cx, swap = 0, 0
+        fidelity_val = 0
+        fidelity_map = Q27_Fake_Auckland()
+        for line in file:
+            arg_list = list(filter(None, line.split(' ')))
+
+            if not len(arg_list) == 6:
+                continue
+
+            if arg_list[1] == "cx":
+                cx += 1
+                fidelity_val += math.log(1 - fidelity_map[f"{arg_list[2]}_{arg_list[3]}"])
+            elif arg_list[1] == "swap":
+                swap += 1
+                fidelity_val += (math.log(1 - fidelity_map[f"{arg_list[2]}_{arg_list[3]}"]) * 3)
+            else:
+                assert False, f"Bad line: {line}!"
+        return cx, swap, fidelity_val
+
+
 def main():
     print("name (other, rz, sx, x, cx, swap)")
     print("original", parse_file_count(filename="./ae_qubit20.qasm"))
@@ -127,6 +149,8 @@ def main():
     print("fidelity")
     print("sabre", fidelity(filename="ae_qubit20_IBM_Q27_FALCON_best_gate_count_forward_swap_149_gate_1411_fidelity-7.69959647088277.qasm"))
     print("rl", fidelity(filename="1g_collect_cost1387_fidelity-28.608579619604157_32810s_env10.qasm"))
+    print("eh fidelity")
+    print("rl-eh", eh_fidelity(filename="rl_eh.txt"))
 
 
 if __name__ == '__main__':
