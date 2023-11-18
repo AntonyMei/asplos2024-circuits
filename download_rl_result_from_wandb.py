c7efe10c4acbc8465a08c3be9686127dcb4e06d0
Yixuan Mei <30572462+AntonyMei@users.noreply.github.com>
2023-11-17 21:16:40 -0500

    + add results for RL on IBM realistic dataset

--- download_rl_result_from_wandb.py
+++ download_rl_result_from_wandb.py
@@ -1,3 +1,5 @@
+import os
+
 import wandb
 
 
@@ -6,6 +8,7 @@ def main():
     max_run_time = 70  # we allow 10s more because of initialization overhead
     collect_run_id = False
     cached_run_id_path = "./RL_results/IBM_Small/run_id.txt"
+    file_save_path = "./RL_results/IBM_Small"
     wandb_project_path = "antonymei/quartz_physical"
     circuit_names = ["ex1_226.qasm", "graycode6_47.qasm", "xor5_254.qasm", "4gt11_84.qasm", "ex-1_166.qasm",
                      "4mod5-v0_20.qasm", "4mod5-v1_22.qasm", "ham3_102.qasm", "mod5d1_63.qasm", "4gt11_83.qasm",
@@ -31,6 +34,13 @@ def main():
                      "majority_239.qasm", "ex2_227.qasm", "cm82a_208.qasm", "sf_274.qasm", "sf_276.qasm", "qft_20.qasm",
                      "con1_216.qasm", "wim_266.qasm", "rd53_130.qasm", "f2_232.qasm", "cm152a_212.qasm",
                      "rd53_251.qasm", "hwb5_53.qasm"]
+
+    rl_1min = [13, 5, 13, 27, 28, 29, 30, 29, 34, 38, 56, 57, 53, 52, 54, 54, 57, 58, 55, 45, 58, 58, 58, 79, 78, 77,
+               85, 92, 77, 111, 107, 115, 111, 114, 115, 108, 109, 132, 137, 135, 138, 139, 110, 151, 166, 184, 176,
+               215, 216, 216, 243, 239, 247, 238, 261, 272, 274, 284, 292, 302, 289, 329, 480, 296, 365, 355, 393, 370,
+               389, 381, 380, 400, 391, 412, 432, 416, 471, 633, 480, 491, 474, 482, 521, 565, 569, 551, 786, 588, 595,
+               685, 674, 767, 751, 796, 809, 836, 858, 963, 902, 1012, 1050, 1099, 1124, 1336, 1333, 1891, 1692, 1751,
+               1847, 2088, 2139, 2302, 2335]
 
     # build the data structures
     circuit_run_id_list = {}
@@ -64,6 +74,19 @@ def main():
                 run_id_list = '[' + line.split('[')[1]
                 circuit_run_id_list[circuit_name] = eval(run_id_list)
                 assert len(circuit_run_id_list[circuit_name]) == 16
+
+    # for each circuit and each run, download qasm files that meets the time limit
+    for circuit_name, best_cost in zip(circuit_names, rl_1min):
+        print(f"Saving files for {circuit_name}, max run time {max_run_time}.")
+        cur_save_dir = os.path.join(file_save_path, circuit_name)
+        for run_id in circuit_run_id_list[circuit_name]:
+            cur_run = api.run(f"{wandb_project_path}/{run_id}")
+            cur_files = cur_run.files()
+            for file in cur_files:
+                if ".qasm" in file.name and f"cost{best_cost}" in file.name:
+                    real_run_time = int(file.name.split('_')[-2][:-1])
+                    if real_run_time <= max_run_time:
+                        file.download(root=cur_save_dir, replace=True)
 
 
 if __name__ == '__main__':
