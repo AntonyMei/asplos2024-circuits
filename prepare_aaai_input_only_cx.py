5749a3792a9de4cbc4485fd4244a7dbcc23a2f46
Yixuan Mei <30572462+AntonyMei@users.noreply.github.com>
2024-03-23 15:23:30 -0400

    + add aaai only cx input

---
+++ prepare_aaai_input_only_cx.py
@@ -0,0 +1,34 @@
+import os
+
+
+def main():
+    circuit_folder_names = [
+        # "qasm27",
+        # "qasm65",
+        "quartz"
+    ]
+    for folder_name in circuit_folder_names:
+        for circuit_name in os.listdir(f"./qasm_files/{folder_name}"):
+            # ignore reversed circuit
+            if "reversed" in circuit_name:
+                continue
+
+            # read the circuit
+            with open(f"./qasm_files/{folder_name}/{circuit_name}") as file:
+                lines = file.readlines()
+
+            # write into new file
+            num_cx = 0
+            with open(f"./aaai_paper/new_circuits/{folder_name}/{circuit_name[:-5]}_onlyCX.qasm", "w") as file:
+                for line in lines:
+                    if line.startswith("//") or line.startswith("OPENQASM") or line.startswith("include") \
+                            or line.startswith("qreg") or line.startswith("creg") or line.startswith("cx") or \
+                            line.startswith("barrier"):
+                        file.write(line)
+                    if line.startswith("cx"):
+                        num_cx += 1
+            print(f"{circuit_name} - {num_cx}")
+
+
+if __name__ == '__main__':
+    main()
